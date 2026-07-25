8471212c2a7f31c452db73548415472a2812bafa
joako-code <pueblajakoo@gmail.com>
2026-07-25 14:12:37 -0300

    fix prisma client generation path

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const prisma = require("./prisma.js");
+
 const app = express();
 const port = 3000;
 
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: "Kanbo API is running" });
+app.get('/', async (req, res) => {
+
+  const users = await prisma.user.findMany();
+
+  res.status(200).json({ message: "Kanbo API is running", users });
 });
 
 app.listen(port,()=>{
-console.log(`Server is runing in port ${port}`);
+console.log(`Server is runing in port http://localhost:${port}`);
 });
 
