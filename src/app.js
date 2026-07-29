eb7429989f2e6cdccc5d3638d20135980096082b
joako-code <pueblajakoo@gmail.com>
2026-07-29 19:55:21 -0300

    remove deprecated import

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const authRoutes = require('./routes/authRoutes')
 const userRoutes = require('./routes/userRoutes')
 const projectRoutes = require('./routes/projectRoutes')
 const taskRoutes = require('./routes/taskRoutes')
-const { authenticate } = require('./middlewares/authMiddleware')
 const swaggerUi = require('swagger-ui-express')
 const swaggerSpec = require('./swagger')
 
@@ -14,7 +13,8 @@ app.use('/auth', authRoutes)
 app.use('/users', userRoutes)
 app.use('/projects', projectRoutes)
 app.use('/tasks', taskRoutes)
+app.use(express.static('public', { etag: false, lastModified: false, cacheControl: false }))
 
-app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 module.exports = app
